c2286852dbc50ce3b274f1b3ad0f801ad8909672
Jürgen Ratzenböck <juergen.ratzenboeck@jobs.at>
2019-09-05 13:23:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/homeaccesscontrol/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.homeaccesscontrol.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
